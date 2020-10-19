3de547b3c13d5ee437779f8eb9ec54fbf2be0abf
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2020-10-19 23:18:38 +0600

    Верстка главной страницы

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+$(window).on("load", function () {
+	$(".preloader").delay(500).fadeOut("slow");
+});
+
 function activeSearchInput() {
 	let inputBlock = document.querySelector(".search_input");
 	inputBlock.classList.toggle("active");
@@ -10,6 +14,23 @@ function mobileCategoryColors() {
 			mobileCategoryArr[i].style.background = '#FFCBA5'
 		} else {
 			mobileCategoryArr[i].style.background = '#A80000'
+		}
+	}
+}
+
+
+
+function resizeCatalogItem() {
+	let catalogItems = document.querySelectorAll(".catalog_item");
+	for (let i = 0; i < catalogItems.length; i++) {
+		if (i % 2 == 0) {
+			let item = catalogItems[i];
+			item.style.marginRight = '-7.5px'
+			item.parentNode.style.marginBottom = '15px'
+		} else {
+			let item = catalogItems[i];
+			item.style.marginLeft = '-7.5px'
+			item.parentNode.style.marginBottom = '15px'
 		}
 	}
 }
@@ -28,4 +49,33 @@ var swiper = new Swiper('.main_slider', {
 		disableOnInteraction: false,
 	},
 });
+
+var doorsSlider = new Swiper('.doorsSlider__container', {
+	slidesPerView: 2,
+	centeredSlides: true,
+	spaceBetween: 30,
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+doorsSlider.slideTo(1, false, false);
+
+
+
+
+
+if (window.innerWidth <= 992) {
+	resizeCatalogItem()
+}
+
+
+
+
+
+
+
+
+
 
